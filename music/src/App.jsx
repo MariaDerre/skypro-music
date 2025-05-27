a038f6d9701db617606cad80df94e06f1bc3ae70
MariaDerre <maria.derre@gmail.com>
2025-05-27 23:27:07 +0300

    audioplayer.style

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -2,71 +2,23 @@ import './App.css'
 import NavMenu from './components/navMenu/navMenu';
 import Tracklist from './components/tracklist';
 import Sidebar from './components/sidebar';
-import AudioPlayer from './components/audioplayer';
-import styled, { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-html,
-body {
-  width: 100%;
-  height: 100%;
-  font-family: "StratosSkyeng", sans-serif;
-  color: #ffffff;
-}
-
-@font-face {
-  font-family: "StratosSkyeng";
-  src: local("StratosSkyeng"), local("StratosSkyeng"),
-    url("../fonts/StratosSkyeng.woff2") format("woff2"),
-    url("../fonts/StratosSkyeng.woff") format("woff");
-  font-weight: 400;
-  font-style: normal;
-}
-
-* {
-  margin: 0;
-  padding: 0;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-*:before,
-*:after {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-a,
-a:visited {
-  text-decoration: none;
-  font-family: "StratosSkyeng", sans-serif;
-  cursor: pointer;
-}
-
-button,
-._btn {
-  cursor: pointer;
-}
-
-ul li {
-  list-style: none;
-}
-`
-
+import AudioPlayer from './components/audioplayer/audioplayer';
+import * as S from './app.style'
 
 function App() {
 return(
-  <div className="wrapper">
-  <div className="container">
-    <main className="main">
+  <S.Wrapper>
+  <S.GlobalStyles/>
+  <S.Container>
+    <S.Main>
       <NavMenu/>
       <Tracklist/>
       <Sidebar/>
-    </main>
+    </S.Main>
     <AudioPlayer/>
     <footer className="footer"></footer>
-  </div>
-</div>
+  </S.Container>
+</S.Wrapper>
 );
 }
 
---
+++ music/src/components/audioplayer/audioplayer.jsx
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import * as S from './audioplayer.style'
+
+// export const  = styled.div``
+
+function AudioPlayer() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setIsLoading(false);
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, []);
+    if (isLoading) {
+      return(
+    <S.Bar>
+      <S.BarContent>
+        <S.BarPlayerProgress></S.BarPlayerProgress>
+        <S.BarPlayerBlock>
+          <S.BarPlayer>
+            <S.PlayerControls>
+              <S.PlayerBtnPrev>
+                <S.PlayerBtnPrevSvg alt="prev">
+                  <use href="img/icon/sprite.svg#icon-prev"></use>
+                </S.PlayerBtnPrevSvg>
+              </S.PlayerBtnPrev>
+              <S.PlayerBtnPlay className="_btn">
+                <S.PlayerBtnPlaySvg alt="play">
+                  <use href="img/icon/sprite.svg#icon-play"></use>
+                </S.PlayerBtnPlaySvg>
+              </S.PlayerBtnPlay>
+              <S.PlayerBtnNext>
+                <S.PlayerBtnNextSvg alt="next">
+                  <use href="img/icon/sprite.svg#icon-next"></use>
+                </S.PlayerBtnNextSvg>
+              </S.PlayerBtnNext>
+              <S.PlayerBtnRepeat className=" _btn-icon">
+                <S.PlayerBtnRepeatSvg alt="repeat">
+                  <use href="img/icon/sprite.svg#icon-repeat"></use>
+                </S.PlayerBtnRepeatSvg>
+              </S.PlayerBtnRepeat>
+              <S.PlayerBtnShuffle className="_btn-icon">
+                <S.PlayerBtnShuffleSvg alt="shuffle">
+                  <use href="../img/icon/sprite.svg#icon-shuffle"></use>
+                </S.PlayerBtnShuffleSvg>
+              </S.PlayerBtnShuffle>
+            </S.PlayerControls>
+
+            <S.PlayerTrackPlay>
+              <S.TrackPlayContain>
+                <S.TrrackPlayImage>
+                <Skeleton width="51px" height="51px" baseColor='#a9a9a9'/>
+                </S.TrrackPlayImage>
+                <S.TrackPlayAuthor>
+                <Skeleton width="60px" height="20px" baseColor='#a9a9a9'/>
+                </S.TrackPlayAuthor>
+                <S.TrackPlayAlbum>
+                <Skeleton width="60px" height="20px" baseColor='#a9a9a9'/>
+                </S.TrackPlayAlbum>
+              </S.TrackPlayContain>
+
+              <S.TrackPlayLikeDis>
+                <S.TrackPlayLike className="_btn-icon">
+                  <S.TrackPlayLikeSvg alt="like">
+                    <use href="img/icon/sprite.svg#icon-like"></use>
+                  </S.TrackPlayLikeSvg>
+                </S.TrackPlayLike>
+                <S.TrackPlayDislike className="_btn-icon">
+                  <S.TrackPlayDisikeSvg alt="dislike">
+                    <use href="img/icon/sprite.svg#icon-dislike"></use>
+                  </S.TrackPlayDisikeSvg>
+                </S.TrackPlayDislike>
+              </S.TrackPlayLikeDis>
+            </S.PlayerTrackPlay>
+          </S.BarPlayer>
+          <S.BarVolumeBlock>
+            <S.VolumeContent>
+              <S.VolumeImage>
+                <S.VolumeSvg alt="volume">
+                  <use href="img/icon/sprite.svg#icon-volume"></use>
+                </S.VolumeSvg>
+              </S.VolumeImage>
+              <S.VolumeProgress className="_btn">
+                <S.VolumeProgressLine
+                  className="_btn"
+                  type="range"
+                  name="range"
+                />
+              </S.VolumeProgress>
+            </S.VolumeContent>
+          </S.BarVolumeBlock>
+        </S.BarPlayerBlock>
+      </S.BarContent>
+    </S.Bar>
+      )
+    }
+
+    return(
+    <S.Bar>
+      <S.BarContent>
+        <S.BarPlayerProgress></S.BarPlayerProgress>
+        <S.BarPlayerBlock>
+          <S.BarPlayer>
+            <S.PlayerControls>
+              <S.PlayerBtnPrev className="_btn">
+                <S.PlayerBtnPrevSvg alt="prev">
+                  <use href="img/icon/sprite.svg#icon-prev"></use>
+                </S.PlayerBtnPrevSvg>
+              </S.PlayerBtnPrev>
+              <S.PlayerBtnPlay className="_btn">
+                <S.PlayerBtnPlaySvg alt="play">
+                  <use href="img/icon/sprite.svg#icon-play"></use>
+                </S.PlayerBtnPlaySvg>
+              </S.PlayerBtnPlay>
+              <S.PlayerBtnNext className="_btn">
+                <S.PlayerBtnNextSvg alt="next">
+                  <use href="img/icon/sprite.svg#icon-next"></use>
+                </S.PlayerBtnNextSvg>
+              </S.PlayerBtnNext>
+              <S.PlayerBtnRepeat className="_btn-icon">
+                <S.PlayerBtnRepeatSvg alt="repeat">
+                  <use href="img/icon/sprite.svg#icon-repeat"></use>
+                </S.PlayerBtnRepeatSvg>
+              </S.PlayerBtnRepeat>
+              <S.PlayerBtnShuffle className=" _btn-icon">
+                <S.PlayerBtnShuffleSvg alt="shuffle">
+                  <use href="../img/icon/sprite.svg#icon-shuffle"></use>
+                </S.PlayerBtnShuffleSvg>
+              </S.PlayerBtnShuffle>
+            </S.PlayerControls>
+
+            <S.PlayerTrackPlay>
+              <S.TrackPlayContain>
+                <S.TrrackPlayImage>
+                  <S.TrackPlaySvg alt="music">
+                    <use href="img/icon/sprite.svg#icon-note"></use>
+                  </S.TrackPlaySvg>
+                </S.TrrackPlayImage>
+                <S.TrackPlayAuthor>
+                  <S.TrackPlayAuthorLink href="http://"
+                    >Ты та...</S.TrackPlayAuthorLink>
+                </S.TrackPlayAuthor>
+                <S.TrackPlayAlbum>
+                  <S.TrackPlayAlbumLink href="http://">Баста</S.TrackPlayAlbumLink>
+                </S.TrackPlayAlbum>
+              </S.TrackPlayContain>
+
+              <S.TrackPlayLikeDis>
+                <S.TrackPlayLike className="_btn-icon">
+                  <S.TrackPlayLikeSvg alt="like">
+                    <use href="img/icon/sprite.svg#icon-like"></use>
+                  </S.TrackPlayLikeSvg>
+                </S.TrackPlayLike>
+                <S.TrackPlayDislike className="_btn-icon">
+                  <S.TrackPlayDisikeSvg alt="dislike">
+                    <use href="img/icon/sprite.svg#icon-dislike"></use>
+                  </S.TrackPlayDisikeSvg>
+                </S.TrackPlayDislike>
+              </S.TrackPlayLikeDis>
+            </S.PlayerTrackPlay>
+          </S.BarPlayer>
+          <S.BarVolumeBlock>
+            <S.VolumeContent>
+              <S.VolumeImage>
+                <S.VolumeSvg alt="volume">
+                  <use href="img/icon/sprite.svg#icon-volume"></use>
+                </S.VolumeSvg>
+              </S.VolumeImage>
+              <S.VolumeProgress className="_btn">
+                <S.VolumeProgressLine
+                  className="_btn"
+                  type="range"
+                  name="range"
+                />
+              </S.VolumeProgress>
+            </S.VolumeContent>
+          </S.BarVolumeBlock>
+        </S.BarPlayerBlock>
+      </S.BarContent>
+    </S.Bar>
+    )
+}
+
+export default AudioPlayer;
